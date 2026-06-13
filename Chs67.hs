5b18cf09166364a3cc2c8d59534a23091e4d6bce
Drew-CSE <143637863+Drew-CSE@users.noreply.github.com>
2026-06-13 19:43:47 -0400

    Add files via upload

---
+++ Chs67.hs
@@ -0,0 +1,23 @@
+module Chs67 where
+
+--don't delete the import, obviously
+--remember to include function types (3 points each)
+
+sumdown _ = 0
+
+
+euclid _ _ = 0 
+
+sum' _ = 0
+
+take' _ _= [] 
+
+last' _ = -1
+
+dec2int' _ = -1
+
+altmap _ _ _ = []
+
+--define your own function(s) to help with luhn
+
+luhn _ = False
